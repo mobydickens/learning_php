30e0dbc6d071671c102eb61784b79a1de63bb5d6
mobydickens <steadmegan@gmail.com>
2019-03-14 15:29:55 -0600

    last commit for the first tutorial

--- laracasts/config.php
+++ laracasts/config.php
@@ -3,10 +3,10 @@
 // configure to make app safer
 return [
   'database' => [
-    'name' => 'mytestdb',
-    'username' => 'root',
-    'password' => 'password',
-    'connection' => 'mysql:host=127.0.0.1',
+    'name' => '',
+    'username' => '',
+    'password' => '',
+    'connection' => '',
     'options' => [
       //error handling
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
